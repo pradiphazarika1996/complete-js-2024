d546528e0c0f7f5a59235212c32289b46c1bc7c1
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-03-05 06:34:36 +0000

    conversion

---
+++ 01_basic/conversionOperator.js
@@ -0,0 +1,20 @@
+const score = null;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+const valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" - 33 number
+// "33abc" - Nan
+// undefined - Nan
+
+// true-1, false - 0;
+
+
+// const isLoggedIn = 1;
+
+// const checkBoolean = Boolean(isLoggedIn);
+// console.log(checkBoolean);
