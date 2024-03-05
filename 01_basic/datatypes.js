da2fafca912c2cdf57db0cf2fc5488c600348491
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-03-05 06:16:46 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,15 @@
+// "use strict"=> treat all js code as a newer version
+
+
+// Number
+// BigInt
+// String
+// float
+// Boolean
+// null => standlone value
+// undefined
+// Symbol - unique
+
+// console.log(typeof null); => return object
+// console.log(typeof undefined); => return undefined
+
