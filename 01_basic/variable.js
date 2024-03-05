7b2f1cedfcd835179a319cd6827017fd244a28b7
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-03-05 05:55:47 +0000

    variable

---
+++ 01_basic/variable.js
@@ -0,0 +1,15 @@
+
+// var a = 10;
+// a = 1.4;
+// let b = 20;
+// const c = 30;
+
+// b = 40;
+// console.log(a);
+
+// let age ;
+// console.log(age);
+
+// var is a global scope- do not use 
+// let is a block scope
+// const is a block scope and value cannot be change
