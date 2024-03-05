2041d536aca5ae5ad5906ecbbaa8206c6bc00f4d
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-03-05 09:48:56 +0000

    string

--- 01_basic/conversionOperator.js
+++ 01_basic/conversionOperator.js
@@ -18,3 +18,38 @@ const valueInNumber = Number(score)
 
 // const checkBoolean = Boolean(isLoggedIn);
 // console.log(checkBoolean);
+
+
+// stack and heap memory example
+// const userOne ={
+//     email:"google.com",
+//     uip:1
+// }
+
+// const userTwo = userOne;
+// userTwo.email ="pradip@gmail.com"
+// console.log(userTwo);
+
+
+// concatination between two String*******************************************************
+
+// const name = "pradip";
+// const age = 27;
+
+// console.log(`my name is ${name.toUpperCase()} and my age is ${age}`);
+
+// const myName = "pradip";
+// console.log(myName.charAt(3));
+// console.log(myName.indexOf('p'));
+// console.log(myName.substring(0,4));
+// const newMyName = myName.slice(-2,1);
+// console.log(newMyName);
+
+// const myName = "  rahul    "
+// console.log(myName);
+// console.log(myName.trim());
+
+// const url = "https://pradip.com/%20/google.com";
+// console.log(url.replace('%20', '-'));
+
+
