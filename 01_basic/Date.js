71164209d9a292b04a6509ba9324132707141df6
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-03-05 11:42:55 +0000

    date

---
+++ 01_basic/Date.js
@@ -0,0 +1,4 @@
+const myDate = new Date();
+
+// console.log(myDate.toJSON());
+console.log(myDate.toDateString(2025,0,23));
---
+++ 01_basic/Num_math.js
@@ -0,0 +1,17 @@
+const balance = 100;
+
+// console.log(balance.toFixed(1));
+// console.log(balance.toLocaleString());
+
+// console.log(Math.min(3,5,6,7,9));
+// console.log(Math.max(3,5,6,7,9));
+// console.log(Math.random());
+// console.log(Math.floor(3.6));
+// console.log(Math.ceil(3.2));
+// console.log(Math.round(3.5));
+
+// const min = 10;
+// const max  = 20;
+
+// console.log(Math.floor(Math.random() * (max-min + 1)+ min));
+
