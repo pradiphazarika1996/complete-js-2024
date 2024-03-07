ce1925f6bca4a923aafe6f753179b52b620c1655
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-03-07 15:21:14 +0000

    object

---
+++ 01_basic/object1.js
@@ -0,0 +1,29 @@
+// const student = {
+    
+//     name:"pradip",
+//     age :34,
+//     designation:"software engineer"
+// }
+
+// console.log(student.name);
+
+// const {name,age,designation} = student;   destructuring object
+// console.log(name,age);
+
+// const arr = ["a", "b", "c"];
+// const newObj = Object.assign({},arr);
+// console.log(newObj);
+// console.log(arr)
+
+
+// convert array into object 
+
+// const arr = ["anil", "akash","vijay"];
+
+// const myObj = {...arr};
+
+// arr.forEach((ele,i)=>{
+//   myObj[`key ${i}`]= ele;
+// })
+
+// console.log(myObj);
