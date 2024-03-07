66f640bd215920890a0ad48c576962bd30457157
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-03-07 14:52:56 +0000

    array

---
+++ 01_basic/Array.js
@@ -0,0 +1,38 @@
+// const num = [1,2,3,4,5];
+// const heroes = new Array("saktiman", "sahid", "ranbir", "anil");
+
+// heroes.push("vivek")
+// heroes.pop();
+// heroes.unshift("rahul")
+// heroes.shift()
+
+// const newArray = heroes.join();
+// console.log(newArray);
+
+// const newNum = num.slice(3);
+// const newNum = num.splice(2)
+// console.log(newNum);
+
+
+// const num = [1,3,4,5,6];
+// num.splice(1,0, 2);
+// console.log(num);
+
+// const cricketer = ["dhoni", "rahul","rahane","pathan", "yuvraj"];
+// const bollywood = ["sharukh","salman", "tiger", "roshan"];
+
+// cricketer.push(bollywood);
+// const result = cricketer.concat(bollywood)
+// const result = [...cricketer, ...bollywood] right approach spread operator
+// console.log(result);
+
+
+// const arr = [1,2,3,[4,5,3,[78,9,7,0,[3,2,16,5]]]]
+// const result = arr.flat(10)
+// console.log(result);
+
+// const num1 = 100;
+// const num2 = 200;
+// const num3 = 300;
+
+// console.log(Array.of(num1,num2,num3));
