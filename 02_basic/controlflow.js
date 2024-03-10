6e808325acdb0e071de4b4d3e6afcf58c5eb7c95
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-03-10 04:35:59 +0000

    control flow

---
+++ 02_basic/controlflow.js
@@ -0,0 +1,33 @@
+// const score = 200;
+
+// if (score <100) {
+//     console.log("executed");
+    
+// } else {
+//     console.log("not executed");
+    
+// }
+
+
+// (score<100)?console.log("executed"):console.log("not executed");  == ternary operator
+
+// const score = 30;
+
+// if(score >100){
+//     console.log("over 100");
+
+// }else if(score >50){
+//     console.log("over 50");
+
+// }else if(score >20){
+//     console.log("over 20");
+
+// }else if(score >9){
+//     console.log("over 9");
+
+// }else{
+//     console.log("over 10");
+
+// }
+
+
