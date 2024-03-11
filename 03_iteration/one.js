1a73677d07bde283df6270504e3b8d0c50e4ce80
pradiphazarika1996 <151366559+pradiphazarika1996@users.noreply.github.com>
2024-03-11 04:10:36 +0000

    loop

---
+++ 03_iteration/one.js
@@ -0,0 +1,23 @@
+// const arr = [1,2,3,4,5,6,7];
+
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i] + 2;
+//     console.log(element);
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop value ${i}`);
+//     for (let j = 1; j <= 10 ; j++) {
+//        console.log(i + "*" + j + "=" + i*j);
+        
+//     }
+    
+// }
